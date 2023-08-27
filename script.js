0bef455d8568360a20fb12ee54723722943bf260
ayushcosmos18 <111158812+ayushcosmos18@users.noreply.github.com>
2023-08-27 21:38:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ document.addEventListener("mousemove", (e) => {
         document.body.appendChild(cursor);
     }
 
-    cursor.style.left = e.clientX + "px";
-    cursor.style.top = e.clientY + "px";
+    cursor.style.left = (e.clientX+20) + "px";
+    cursor.style.top = (e.clientY+20) + "px";
 });
 
 document.addEventListener("mouseout", () => {
@@ -39,3 +39,30 @@ document.addEventListener("mouseout", () => {
         cursor = null;
     }
 });
+
+
+var navlinks=document.getElementById("navlinks");
+function showmenu()
+{
+    navlinks.style.right="0";
+}
+function hidemenu()
+{
+    navlinks.style.right="-150px"
+}
+
+
+document.getElementById("clicableDiv1").addEventListener("click", function() {
+    window.location.href = "blogs.html"; // Replace with your desired link
+  });
+  
+
+  
+document.getElementById("clicableDiv2").addEventListener("click", function() {
+    window.location.href = "blogs2.html"; // Replace with your desired link
+  });
+  
+  document.getElementById("clicableDiv3").addEventListener("click", function() {
+    window.location.href = "blogs3.html"; // Replace with your desired link
+  });
+  
